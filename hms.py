a9da17f71d5dee386fb1e6060e30ff52e3689036
Dorota Nowicz <nowiczka@gmail.com>
2019-07-27 12:14:39 +0200

    Create hms.py

---
+++ hms.py
@@ -0,0 +1,8 @@
+"""
+This script opens the project "Project 1",and conducts compute named "Run 1".
+"""
+
+from hms.model.Jython Hms import *
+OpenProject("Project 1","C:\Project")
+Compute ("Run 1")
+Exit(1)
